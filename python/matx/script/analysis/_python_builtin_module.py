db953c68220f1ed8ba5bf0906289902fb1b66aeb
maxiandi <maxiandi@bytedance.com>
2022-11-10 13:17:44 +0800

    [script] fix conflict with local modules

--- python/matx/script/analysis/_python_builtin_module.py
+++ python/matx/script/analysis/_python_builtin_module.py
@@ -16,128 +16,92 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
-import builtins
-import abc
-import argparse
-import array
-import asyncio
-import ast
-import atexit
-import base64
-import binascii
-import binhex
-import bisect
-import bz2
-import collections
-import copy
-import csv
-import configparser
-import cmath
-import ctypes
-import datetime
-import decimal
-import functools
-import gc
-import getopt
-import gzip
-import hashlib
-import html
-import http
-import importlib
-import io
-import ipaddress
-import itertools
-import inspect
-import json
-import logging
-import math
-import multiprocessing
 import os
-import pdb
-import random
-import re
-import shutil
 import sys
-import string
-import struct
-import socket
-import statistics
-import threading
-import types
-import unittest
-import urllib
-import uuid
-import typing
-import unicodedata
-import time
-import timeit
-import trace
-import traceback
-import xml
-import zipfile
-import zlib
 
-BUILTIN_MODULES = [
-    builtins,
-    abc,
-    argparse,
-    array,
-    ast,
-    asyncio,
-    atexit,
-    base64,
-    binascii,
-    binhex,
-    bisect,
-    bz2,
-    collections,
-    configparser,
-    cmath,
-    copy,
-    csv,
-    ctypes,
-    datetime,
-    decimal,
-    functools,
-    gc,
-    getopt,
-    gzip,
-    hashlib,
-    html,
-    http,
-    importlib,
-    io,
-    ipaddress,
-    itertools,
-    inspect,
-    json,
-    logging,
-    math,
-    multiprocessing,
-    os,
-    pdb,
-    random,
-    re,
-    shutil,
-    sys,
-    string,
-    struct,
-    socket,
-    statistics,
-    threading,
-    types,
-    unittest,
-    urllib,
-    uuid,
-    typing,
-    unicodedata,
-    time,
-    timeit,
-    trace,
-    traceback,
-    threading,
-    xml,
-    zipfile,
-    zlib,
+BUILTIN_MODULE_NAMES = [
+    "builtins",
+    "abc",
+    "argparse",
+    "array",
+    "ast",
+    "asyncio",
+    "atexit",
+    "base64",
+    "binascii",
+    "binhex",
+    "bisect",
+    "bz2",
+    "collections",
+    "configparser",
+    "cmath",
+    "copy",
+    "csv",
+    "ctypes",
+    "datetime",
+    "decimal",
+    "functools",
+    "gc",
+    "getopt",
+    "gzip",
+    "hashlib",
+    "html",
+    "http",
+    "importlib",
+    "io",
+    "ipaddress",
+    "itertools",
+    "inspect",
+    "json",
+    "logging",
+    "math",
+    "multiprocessing",
+    "os",
+    "pdb",
+    "random",
+    "re",
+    "shutil",
+    "sys",
+    "string",
+    "struct",
+    "socket",
+    "statistics",
+    "threading",
+    "types",
+    "unittest",
+    "urllib",
+    "uuid",
+    "typing",
+    "unicodedata",
+    "time",
+    "timeit",
+    "trace",
+    "traceback",
+    "threading",
+    "xml",
+    "zipfile",
+    "zlib",
 ]
+
+
+def __get_python_builtin_module_path():
+    site = sys.modules['site']
+    mod_path = os.path.abspath(os.path.split(os.path.realpath(site.__file__))[0])
+    return mod_path, site.getsitepackages()[0]
+
+
+BUILTIN_MODULE_PATH, SITE_PACKAGES_PATH = __get_python_builtin_module_path()
+
+
+def is_builtin_module(mod):
+    mod_name = mod.__name__
+    if mod_name in sys.builtin_module_names:
+        return True
+    mod_file = getattr(mod, "__file__", None)
+    if mod_file is None:
+        return True
+    if mod_name in BUILTIN_MODULE_NAMES:
+        mod_path = os.path.abspath(os.path.split(os.path.realpath(mod_file))[0])
+        if mod_path.startswith(BUILTIN_MODULE_PATH) and not mod_path.startswith(SITE_PACKAGES_PATH):
+            return True
+    return False
