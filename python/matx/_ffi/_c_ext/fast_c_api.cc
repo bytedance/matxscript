0e88ec02d3611a51902a5d59c76081232ecbbac0
Chi Zhang <zhangchi.usc1992@bytedance.com>
2023-02-03 11:40:27 +0800

    Update fast_c_api.cc

--- python/matx/_ffi/_c_ext/fast_c_api.cc
+++ python/matx/_ffi/_c_ext/fast_c_api.cc
@@ -955,7 +955,6 @@ static PyObject* MATXScriptAnySwitchToObject(MATXScriptAny* value) {
     PyObjectMATXScriptObjectBase* super = (PyObjectMATXScriptObjectBase*)(result);
     super->handle = value->data.v_handle;
     super->type_code = value->code;
-    
     PyObject* ret = nullptr;
     for (int i = 0; i < OBJECT_CALLBACK_CUR_IDX; ++i) {
       if (OBJECT_CALLBACK_TABLE[i].index == value->code) {
