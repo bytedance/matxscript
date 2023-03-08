9b3e02d45638c6f5a886ab76483b438a9eac3d67
jiachengyang <jiachengyang@bytedance.com>
2023-03-08 15:46:50 -0800

    fix a kernel_type print bug

--- python/matx/kernel/utils.py
+++ python/matx/kernel/utils.py
@@ -17,35 +17,3 @@
 #   * specific language governing permissions and limitations
 #   * under the License.
 #   */
-
-
-from ..symbol import is_symbol
-
-
-class NDArrayType:
-    def __init__(self, shape, dtype):
-        self.shape = shape
-        self.dtype = dtype
-        self.storage = 'cpu'
-        self.symbol_list = [axis for axis in shape if is_symbol(axis)]
-
-    def __repr__(self):
-        return f'NDArrayType (dtype={self.dtype}, shape={self.shape})'
-
-
-class ScalarType(NDArrayType):
-
-    def __init__(self, dtype):
-        super().__init__([1], dtype)
-
-    def __getitem__(self, shape) -> NDArrayType:
-        if isinstance(shape, list) or isinstance(shape, tuple):
-            if list(shape) == [1]:
-                return self
-            return NDArrayType(tuple(shape), self.dtype)
-        if shape == 1:
-            return self
-        return NDArrayType((shape,), self.dtype)
-
-    def __repr__(self) -> str:
-        return f'ScalarType (dtype={self.dtype}, storage={self.storage})'
