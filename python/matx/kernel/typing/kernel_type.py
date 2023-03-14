453654b2d9d945d004c91690392f6d12dd9a041b
jiachengyang <jiachengyang@bytedance.com>
2023-03-14 16:12:40 -0700

    mvp development. ready for debugging

--- python/matx/kernel/typing/kernel_type.py
+++ python/matx/kernel/typing/kernel_type.py
@@ -1,25 +1,44 @@
-#  // Copyright 2023 ByteDance Ltd. and/or its affiliates.
-#  /*
-#   * Licensed to the Apache Software Foundation (ASF) under one
-#   * or more contributor license agreements.  See the NOTICE file
-#   * distributed with this work for additional information
-#   * regarding copyright ownership.  The ASF licenses this file
-#   * to you under the Apache License, Version 2.0 (the
-#   * "License"); you may not use this file except in compliance
-#   * with the License.  You may obtain a copy of the License at
-#   *
-#   *   http://www.apache.org/licenses/LICENSE-2.0
-#   *
-#   * Unless required by applicable law or agreed to in writing,
-#   * software distributed under the License is distributed on an
-#   * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-#   * KIND, either express or implied.  See the License for the
-#   * specific language governing permissions and limitations
-#   * under the License.
-#   */
+#  Copyright 2023 ByteDance Ltd. and/or its affiliates.
+#
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
 
+import numpy as np
 
 from ..symbol import is_symbol
+
+NPDTYPE_TO_STR = {
+    np.bool_: 'bool',
+    np.int8: 'int8',
+    np.int16: 'int16',
+    np.int32: 'int32',
+    np.int64: 'int64',
+    np.intc: 'int32',
+    np.uint8: 'uint8',
+    np.uint16: ' uint16',
+    np.uint32: ' uint32',
+    np.uint64: ' uint64',
+    np.uintc: 'uint32',
+    np.float16: 'float16',
+    np.float32: 'float32',
+    np.float64: 'float64',
+    np.longlong: 'int64',
+    np.ulonglong: ' uint64'
+}
 
 
 class NDArrayType:
@@ -31,6 +50,9 @@ class NDArrayType:
 
     def __repr__(self):
         return f'NDArrayType (dtype={self.dtype}, shape={self.shape})'
+
+    def dtype_str(self):
+        return NPDTYPE_TO_STR[self.dtype]
 
 
 class ScalarType(NDArrayType):
