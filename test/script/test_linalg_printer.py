11f61d0258ee60496c180ed4776398548a6ee182
jiachengyang <jiachengyang@bytedance.com>
2023-04-20 15:12:31 -0700

    add more test

--- test/script/test_linalg_printer.py
+++ test/script/test_linalg_printer.py
@@ -24,7 +24,7 @@ import unittest
 
 class TestLinalgStatementPrint(unittest.TestCase):
     # (a+b)(c-d)/e
-    def test_basic_arith_op(self):
+    def test_float_arith_op(self):
         a = matx.ir.PrimVar("a", "float32")
         b = matx.ir.PrimVar("b", "float32")
         t1 = matx.ir.PrimAdd(a, b)
@@ -40,7 +40,8 @@ class TestLinalgStatementPrint(unittest.TestCase):
         func_name = "basic_arith_op"
         prim_func = prim_func.with_attr("global_symbol", func_name)
         linalg_statement = _ffi_node_api.as_linalg_text(prim_func).decode()
-        expected_statement = """func.func @basic_arith_op(%a: f32, %b: f32, %c: f32, %d: f32, %e: f32)->f32{
+        expected_statement = """
+func.func @basic_arith_op(%a: f32, %b: f32, %c: f32, %d: f32, %e: f32)->f32{
 %0 = arith.addf %a, %b : f32
 %1 = arith.subf %c, %d : f32
 %2 = arith.mulf %0, %1 : f32
@@ -48,6 +49,34 @@ class TestLinalgStatementPrint(unittest.TestCase):
 func.return %3 :f32
 }"""
         self.assertEqual(expected_statement.strip(), linalg_statement.strip())
+
+    def test_int_arith_op(self):
+        a = matx.ir.PrimVar("a", "int32")
+        b = matx.ir.PrimVar("b", "int32")
+        t1 = matx.ir.PrimAdd(a, b)
+        c = matx.ir.PrimVar("c", "int32")
+        d = matx.ir.PrimVar("d", "int32")
+        t2 = matx.ir.PrimSub(c, d)
+        t3 = matx.ir.PrimMul(t1, t2)
+        e = matx.ir.PrimVar("e", "int32")
+        t4 = matx.ir.PrimDiv(t3, e)
+        ib = matx.ir.ir_builder.create()
+        ib.emit(matx.ir.ReturnStmt(t4))
+        prim_func = matx.ir.PrimFunc([a, b, c, d, e], [], ib.get(), matx.ir.PrimType("int32"))
+        func_name = "basic_arith_op"
+        prim_func = prim_func.with_attr("global_symbol", func_name)
+        linalg_statement = _ffi_node_api.as_linalg_text(prim_func).decode()
+        expected_statement = """
+func.func @basic_arith_op(%a: i32, %b: i32, %c: i32, %d: i32, %e: i32)->i32{
+%0 = arith.addi %a, %b : i32
+%1 = arith.subi %c, %d : i32
+%2 = arith.muli %0, %1 : i32
+%3 = arith.divi %2, %e : i32
+func.return %3 :i32
+}"""
+        self.assertEqual(expected_statement.strip(), linalg_statement.strip())
+
+
 
 
 if __name__ == "__main__":
