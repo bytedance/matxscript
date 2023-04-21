0d1352e57cb7d52f965b06e17d58200e8a795752
jiachengyang <jiachengyang@bytedance.com>
2023-04-21 12:58:30 -0700

    more tests

--- test/script/test_linalg_printer.py
+++ test/script/test_linalg_printer.py
@@ -37,11 +37,11 @@ class TestLinalgStatementPrint(unittest.TestCase):
         ib = matx.ir.ir_builder.create()
         ib.emit(matx.ir.ReturnStmt(t4))
         prim_func = matx.ir.PrimFunc([a, b, c, d, e], [], ib.get(), matx.ir.PrimType("float32"))
-        func_name = "basic_arith_op"
+        func_name = "test_float_arith_op"
         prim_func = prim_func.with_attr("global_symbol", func_name)
         linalg_statement = _ffi_node_api.as_linalg_text(prim_func).decode()
         expected_statement = """
-func.func @basic_arith_op(%a: f32, %b: f32, %c: f32, %d: f32, %e: f32)->f32{
+func.func @test_float_arith_op(%a: f32, %b: f32, %c: f32, %d: f32, %e: f32)->f32{
 %0 = arith.addf %a, %b : f32
 %1 = arith.subf %c, %d : f32
 %2 = arith.mulf %0, %1 : f32
@@ -63,11 +63,11 @@ func.return %3 :f32
         ib = matx.ir.ir_builder.create()
         ib.emit(matx.ir.ReturnStmt(t4))
         prim_func = matx.ir.PrimFunc([a, b, c, d, e], [], ib.get(), matx.ir.PrimType("int32"))
-        func_name = "basic_arith_op"
+        func_name = "test_int_arith_op"
         prim_func = prim_func.with_attr("global_symbol", func_name)
         linalg_statement = _ffi_node_api.as_linalg_text(prim_func).decode()
         expected_statement = """
-func.func @basic_arith_op(%a: i32, %b: i32, %c: i32, %d: i32, %e: i32)->i32{
+func.func @test_int_arith_op(%a: i32, %b: i32, %c: i32, %d: i32, %e: i32)->i32{
 %0 = arith.addi %a, %b : i32
 %1 = arith.subi %c, %d : i32
 %2 = arith.muli %0, %1 : i32
@@ -76,8 +76,23 @@ func.return %3 :i32
 }"""
         self.assertEqual(expected_statement.strip(), linalg_statement.strip())
 
-    def test_pointer_op(self):
+    def test_pointer_op_memref_i32(self):
         ptr_type = matx.ir.PointerType(matx.ir.PrimType("int32"))
+        a = matx.ir.PrimVar("a", ptr_type)
+        ib = matx.ir.ir_builder.create()
+        ib.emit(matx.ir.ReturnStmt(a))
+        prim_func = matx.ir.PrimFunc([a], [], ib.get(), ptr_type)
+        func_name = "test_pointer_op_memref_i32"
+        prim_func = prim_func.with_attr("global_symbol", func_name)
+        linalg_statement = _ffi_node_api.as_linalg_text(prim_func).decode()
+        expected_statement = """
+func.func @test_pointer_op_memref_i32(%a: memref<?xi32>)->memref<?xi32>{
+func.return %a :memref<?xi32>
+} """
+        self.assertEqual(expected_statement.strip(), linalg_statement.strip())
+
+    def test_pointer_op_memref_f64(self):
+        ptr_type = matx.ir.PointerType(matx.ir.PrimType("float64"))
         a = matx.ir.PrimVar("a", ptr_type)
         ib = matx.ir.ir_builder.create()
         ib.emit(matx.ir.ReturnStmt(a))
@@ -86,8 +101,8 @@ func.return %3 :i32
         prim_func = prim_func.with_attr("global_symbol", func_name)
         linalg_statement = _ffi_node_api.as_linalg_text(prim_func).decode()
         expected_statement = """
-func.func @basic_arith_op(%a: memref<?xi32>)->memref<?xi32>{
-func.return %a :memref<?xi32>
+func.func @basic_arith_op(%a: memref<?xf64>)->memref<?xf64>{
+func.return %a :memref<?xf64>
 } """
         self.assertEqual(expected_statement.strip(), linalg_statement.strip())
 
