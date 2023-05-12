02884dd5bb134678d406bea01838bd47b65d4ad7
jiachengyang <jiachengyang@bytedance.com>
2023-05-11 19:59:18 -0700

    fixed a bug that did not use the right mlir cast statements

--- src/ir/printer/kernel_printer/mlir_printer.cc
+++ src/ir/printer/kernel_printer/mlir_printer.cc
@@ -197,7 +197,8 @@ std::pair<std::string, std::string> MLIRTextPrinter::GetNodeDataType(const PrimE
   auto op_dtype = op->dtype.code();
   auto bits = op->dtype.bits();
   switch (op->dtype.code()) {
-    case kDLInt:
+  case kDLInt:
+  case kDLUInt:
       arith_suffix = "i";
       break;
     case kDLFloat:
@@ -311,10 +312,62 @@ void MLIRTextPrinter::VisitExpr_(const PrimLetNode* op, std::ostream& os) {
 void MLIRTextPrinter::VisitExpr_(const PrimCallNode* op, std::ostream& os) {
 }
 
+void printCastOp(const Type &origin, const Type &target, std::ostream& os) {
+  /**
+   * arith.extf - cast from floating-point to wider floating-point
+   * arith.extsi - integer sign extension operation
+   * arith.extui - integer zero extension operation
+   * arith.fptosi - cast from floating-point type to signed integer type
+   * arith.fptoui - cast from floating-point type to unsigned integer type
+   * arith.sitofp - cast from signed integer type to floating-point
+   * arith.truncf - cast from floating-point to narrower floating-point
+   * arith.trunci - integer truncation operation
+   * arith.uitofp - cast from unsigned integer type to floating-point
+   */
+  auto* origin_t = origin.as<PrimTypeNode>();
+  auto* target_t = target.as<PrimTypeNode>();
+  if (origin_t==nullptr || target_t==nullptr){
+    MXTHROW << "[MLIR] casting between non prim type node is not allowed";
+  }
+  auto const origin_t_bits = origin_t->dtype.bits();
+  auto const origin_t_code = origin_t->dtype.code();
+  auto const target_t_bits = target_t->dtype.bits();
+  auto const target_t_code = target_t->dtype.code();
+  if (origin_t_code != kDLInt && origin_t_code != kDLUInt && origin_t_code != kDLFloat){
+    MXTHROW << "[MLIR] the type being casted from is neither int nor float";
+  }
+  if (target_t_code != kDLInt && target_t_code != kDLUInt && target_t_code != kDLFloat){
+    MXTHROW << "[MLIR] the type being casted to is neither int nor float";
+  }
+  if (origin_t_code == target_t_code && origin_t_bits == target_t_bits){
+    MXTHROW << "[MLIR] casting between the same type is not allowed";
+  }
+  int compare = 0;
+  if (target_t_bits<origin_t_bits){
+    compare = 0;
+  }else if (target_t_bits == origin_t_bits){
+    compare = 1;
+  }else {
+    compare = 2;
+  }
+  /**
+   *  {{{"i i t<o",  "i i t=o",  "i i t>o"},  {"i ui t<o",  "i ui t=o",  "i ui t>o"},  {"i f t<o",   "i f t=o",  "i f t>o"}}},
+   *   {{"ui i t<o", "ui i t=o", "ui i t>o"}, {"ui ui t<o", "ui ui t=o", "ui ui t>o"}, {"ui f t<o",  "ui f t=o", "ui f t>o"}},
+   *   {{"f i t<o",  "f i t=o",  "f i t>o"},  {"f ui t<o",  "f ui t=o",  "f ui t>o"},  {"f f t<o",   "f f t=o",  "f f t>o"}}}
+   */
+
+  static const std::string op_map[3][3][3] = {{{"arith.trunci", "arith.bitcast", "arith.extsi"},  {"arith.trunci", "arith.bitcast", "arith.extui"},  {"arith.sitofp", "arith.sitofp",  "arith.sitofp"}},
+                                              {{"arith.trunci", "arith.bitcast", "arith.extui"},  {"arith.trunci", "arith.bitcast", "arith.extui"},  {"arith.uitofp", "arith.uitofp",  "arith.uitofp"}},
+                                              {{"arith.fptosi", "arith.fptosi",  "arith.fptosi"}, {"arith.fptoui", "arith.fptoui",  "arith.fptoui"}, {"arith.truncf", "arith.bitcast", "arith.extf"}}};
+  os << op_map[origin_t_code][target_t_code][compare]<<' ';
+}
+
+
 void MLIRTextPrinter::VisitExpr_(const PrimCastNode* op, std::ostream& os) {
   auto& v = op->value;
   PrimExprFunctor::VisitExpr(v, os);
-  os << '%' << cur_index_ << " = unrealized_conversion_cast ";
+  os << '%' << cur_index_ << " = ";
+  printCastOp(v->checked_type(), op->checked_type(), os);
   PrintNodeName(v, os);
   os << " : ";
   os << ConvertTypeToMLIR(v->checked_type()) << " to " << ConvertTypeToMLIR(op->checked_type())
