8da4c6084ac15275cfac601b93d1da8526760545
jiachengyang <jiachengyang@bytedance.com>
2023-06-12 21:03:23 -0700

    support scalar allocation

---
+++ test/kernel/test_scalar_functions.py
@@ -0,0 +1,54 @@
+#  Copyright 2023 ByteDance Ltd. and/or its affiliates.
+#
+#  Licensed to the Apache Software Foundation (ASF) under one
+#  or more contributor license agreements.  See the NOTICE file
+#  distributed with this work for additional information
+#  regarding copyright ownership.  The ASF licenses this file
+#  to you under the Apache License, Version 2.0 (the
+#  "License"); you may not use this file except in compliance
+#  with the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing,
+#  software distributed under the License is distributed on an
+#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#  KIND, either express or implied.  See the License for the
+#  specific language governing permissions and limitations
+#  under the License.
+
+import unittest
+
+from matx.kernel.kernel_parser import KernelParser
+from matx.kernel.compile_linalg import compile_linalg
+from matx.kernel.typing import int32, float32, float64, boolean
+
+
+class TestMLIRIntArithmeticOp(unittest.TestCase):
+    def helper(self, foo):
+        p = KernelParser(foo)
+        p.parse()
+        print()
+        print("=" * 30, "linalg_code", "=" * 30, sep="")
+        print()
+        print(p.linalg_code())
+        print()
+        print("=" * 30, "compile and run", "=" * 30, sep="")
+        print()
+        f = compile_linalg(p)
+        return f
+
+    def test_int_add(self):
+        def foo(a: int32, b: int32) -> int32:
+            c:int32 = a+b
+            return a + c
+
+        foo = self.helper(foo)
+
+
+if __name__ == "__main__":
+    import logging
+
+    logging.basicConfig(level=logging.INFO)
+    unittest.main()
+    
