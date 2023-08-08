483ecb3892e3601427efb155f9d3c079defce3b4
jiachengyang <jiachengyang@bytedance.com>
2023-08-08 15:38:08 -0700

    update the typing utility

--- python/matx/kernel/typing/utils.py
+++ python/matx/kernel/typing/utils.py
@@ -20,11 +20,11 @@
 
 
 import numbers
+import typing
 
-import numpy as np
 import sympy
 
-from matx.kernel.symbol.utils import is_symbol, is_symbol_type
+from matx.kernel.symbol.utils import is_symbol
 from matx.kernel.typing.kernel_type import NDArrayType
 from matx.kernel.typing.type_def import *
 
@@ -98,3 +98,7 @@ def np_result_dtype(nptypes):
             if k == restype.type:
                 return k
     return restype.type
+
+
+def is_dynamic_ndarray(t):
+    return isinstance(t, NDArrayType) and any(i == typing.Any for i in t.shape)
