66ad9e93354c51523562bce766f34457ebdbde0d
jiachengyang <jiachengyang@bytedance.com>
2023-08-18 00:38:37 -0700

    code format and fix ci bug

--- src/runtime/mlir/convert_memref.cpp
+++ src/runtime/mlir/convert_memref.cpp
@@ -17,8 +17,8 @@
 // specific language governing permissions and limitations
 // under the License.
 
-#include "matxscript/runtime/mlir/convert_memref.h"
 #include <unordered_map>
+#include "matxscript/runtime/mlir/convert_memref.h"
 #include "matxscript/runtime/mlir/memref_cpp_interface.h"
 
 template <typename src_t, typename dst_t>
